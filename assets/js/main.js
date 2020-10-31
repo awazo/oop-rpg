daf95c83ad30afb1d6b887e80a703473dd3f8ed6
ozawa hiroshi <hiroshi.ozawa@systems.tocca-net.jp>
2020-11-01 01:04:58 +0900

    fix syntax error

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 import { default as Model } from './model/model.js';
 import { default as Controller } from './controller/controller.js';
 
-Controller.Game.start(Model);
+(new Controller.Game()).start(Model);
 
