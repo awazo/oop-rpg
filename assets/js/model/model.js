aace14b5a2d09e791941b18eba8436b5cef8bf5e
ozawa hiroshi <hiroshi.ozawa@systems.tocca-net.jp>
2020-11-01 01:22:31 +0900

    fix error

--- assets/js/model/model.js
+++ assets/js/model/model.js
@@ -7,10 +7,6 @@ Model.Character = Character;
 Model.test = function() {
   console.log('hoge');
   var human = new Model.Character.Human();
-  console.log(human);
-  console.log(human.prototype);
-  console.log(human.showStatus);
-  console.log(human.prototype.showStatus);
   human.showStatus();
 };
 
